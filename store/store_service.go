04c839b8729bf931abe134cb4d73c2fae677fc6a
thales-pinner <thales.neves@pinpeople.com.br>
2024-05-12 21:49:59 -0300

    Add methods to save and retrieve url on redis

--- store/store_service.go
+++ store/store_service.go
@@ -41,3 +41,25 @@ func NewStoreService() *StorageService {
 	storeService.redisClient = redisClient
 	return storeService
 }
+
+// We want to be able to save the mapping between the originalUrl and the generated shortUrl url
+
+func SaveUrlMapping(shortUrl string, originalUrl string, userId string) {
+	err := storeService.redisClient.Set(ctx, shortUrl, originalUrl, CacheDuration).Err()
+
+	if err != nil {
+		panic(fmt.Sprintf("Failed saving url Key | Error: %v - shortUrl: %s - originalUrl: %s\n", err, shortUrl, originalUrl))
+	}
+}
+
+// We should be able to retrieve the initial long URL once the short is provbided.
+
+func RetrieveInitialUrl(shortUrl string) string {
+	result, err := storeService.redisClient.Get(ctx, shortUrl).Result()
+
+	if err != nil {
+		panic(fmt.Sprintf("Failed retrieving Original URL | Error: %v - shortUrl: %s/n", err, shortUrl))
+	}
+
+	return result
+}
